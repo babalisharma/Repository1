ba6307bd6cf692ec20ba50f6e45e378877922f0f
Pradeep <Pradeep@192.168.0.100>
2018-05-21 17:13:27 +0530

    commit3

--- github/src/test/java/github/Script.java
+++ github/src/test/java/github/Script.java
@@ -2,7 +2,8 @@ package github;
 
 public class Script {
 	public static void main(String[] args) {
-		System.out.println("it is maven project");
+		//System.out.println("it is maven project");
+		System.out.println("my name babali");
 		
 	}
 
